9da7ca474b9bfa2a9cb871681f5ef63aac6d3609
Jesbin <jesbin@techfriar.com>
2023-07-13 17:18:43 +0530

    bug fix

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,6 +1,6 @@
 import express from 'express'
-import { addMessage} from '../controllers/contactUsController.js';
+import { publicAddMessage} from '../controllers/contactUsController.js';
 
 const router = express.Router();
-router.route('/add').post(addMessage);
+router.route('/add').post(publicAddMessage);
 export default router;
